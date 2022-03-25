e6fbf92aaf0f577848cd0639bf1c89ddcb451da3
IAlibay <ialibay@mdanalysis.org>
2022-03-25 18:50:14 +0000

    chemical states make up the states which are joined by alchemical states

--- gufe/__init__.py
+++ gufe/__init__.py
@@ -2,8 +2,7 @@
 from . import _version
 __version__ = _version.get_versions()['version']
 
-from .component import Component
 from .ligandcomponent import LigandComponent
 from .proteincomponent import ProteinComponent
 from .solventcomponent import SolventComponent
-from .microstate import Microstate
+from .chemicalstate import ChemicalState
--- gufe/chemicalstate.py
+++ gufe/chemicalstate.py
@@ -1,6 +1,6 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/gufe
 
-class Microstate:
+class ChemicalState:
     pass
     # TODO: DD Magic
