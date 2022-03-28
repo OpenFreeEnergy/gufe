5bb8d57b863906eca0029bb246bb6cda73ba4b27
David Dotson <dotsdl@gmail.com>
2022-03-28 16:50:14 -0700

    Removed frozendict from chemicalstate usage

--- gufe/chemicalstate.py
+++ gufe/chemicalstate.py
@@ -4,9 +4,8 @@
 from typing import Dict, Optional
 
 import numpy as np
-from frozendict import frozendict
 
-from .custom_typing import RDKitMol
+from .component import Component
 
 
 class ChemicalState:
@@ -17,23 +16,23 @@ class ChemicalState:
     components
         The molecular representation of the chemical state, including
         connectivity and coordinates. This is a frozendict with user-defined
-        labels as keys, RDKit molecules as values.
+        labels as keys, `Component`s as values.
     box_vectors
         Numpy array indicating shape and size of unit cell for the system. May
         be a partial definition to allow for variability on certain dimensions.
     identifier
         Optional identifier for the chemical state; used as part of the
         (hashable) graph node itself when the chemical state is added to an
-        `AlchemicalNetwork`
+        `AlchemicalNetwork`.
 
     """
 
     def __init__(
-            self,
-            components: Dict[str, RDKitMol],
-            box_vectors: Optional[np.ndarray] = None,
-            identifier: Optional[str] = None,
-            ):
+        self,
+        components: Dict[str, Component],
+        box_vectors: Optional[np.ndarray] = None,
+        identifier: Optional[str] = None,
+    ):
         """Create a node for an alchemical network.
 
         Attributes
@@ -41,31 +40,37 @@ class ChemicalState:
         components
             The molecular representation of the chemical state, including
             connectivity and coordinates. Given as a dict with user-defined
-            labels as keys, RDKit molecules as values.
+            labels as keys, `Component`s as values.
         box_vectors
-            Numpy array indicating shape and size of unit cell for the system.
-            May be a partial definition to allow for variability on certain
-            dimensions.
+            Optional `numpy` array indicating shape and size of unit cell for
+            the system. May be a partial definition to allow for variability on
+            certain dimensions.
         identifier
-            Optional identifier for the chemical state; used as part of the
-            (hashable) graph node itself when the chemical state is added to an
-            `AlchemicalNetwork`
+            Optional identifier for the chemical state; included with the other
+            attributes as part of the (hashable) graph node itself when the
+            chemical state is added to an `AlchemicalNetwork`.
 
         """
-        self._components = frozendict(components)
+        self._components = components
         self._identifier = identifier
 
         if box_vectors is None:
-            self._box_vectors = np.array([np.nan]*9)
+            self._box_vectors = np.array([np.nan] * 9)
         else:
             self._box_vectors = box_vectors
 
     def __hash__(self):
-        return hash((self._components, self._identifier, self._box_vectors))
+        return hash(
+            (
+                tuple(self._components.items()),
+                self._box_vectors,
+                self._identifier,
+            )
+        )
 
     @property
     def components(self):
-        return self._components
+        return dict(self._components)
 
     @property
     def box_vectors(self):
@@ -75,17 +80,19 @@ class ChemicalState:
     def identifier(self):
         return self._identifier
 
-    @classmethod
-    def as_protein_ligand_solvent(cls):
-        """
+    @property
+    def charge(self):
+        """Total charge for the ChemicalState."""
+        return sum([component.charge for component in self._components.values()])
 
-        """
+    @classmethod
+    def as_protein_smallmolecule_solvent(cls):
+        """ """
+        # alternate initializer for typical protein+ligand+solvent system
         ...
 
-
     @classmethod
-    def as_ligand_solvent(cls):
-        """
-    
-        """
+    def as_smallmolecule_solvent(cls):
+        """ """
+        # alternate initializer for typical ligand+solvent system
         ...
