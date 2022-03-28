3441b25cef1a5dfcf41ff50bcc905a3c883fa8a9
richard <richardjgowers@gmail.com>
2022-03-28 16:47:40 +0100

    add openff.units as dependency
    solvents:
    
    add units to concentration
    
    add neutralize flag

--- gufe/solventcomponent.py
+++ gufe/solventcomponent.py
@@ -1,26 +1,35 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/gufe
-import dataclasses
-from dataclasses import dataclass
 from typing import Tuple
+from openff.units import unit
 
 
 # really wanted to make this a dataclass but then can't sort & strip ion input
 class SolventComponent:
-    """Solvent molecules
+    """Solvent molecules in a chemical state
 
-    Used to fully represent the chemical state being expressed.
+    This component represents the abstract idea of the solvent and ions present
+    around the other components, rather than a list of specific water molecules
+    and their coordinates.  This abstract representation is later made concrete
+    by specific methods.
     """
-    def __init__(self, smiles='O', ions=None, concentration=None):
+    def __init__(self, smiles: str = 'O',
+                 ions: Tuple[str, ...] = None,
+                 neutralize: bool = True,
+                 concentration: unit.Quantity = None):
         """
         Parameters
         ----------
         smiles : str, optional
           smiles of the solvent, default 'O' (water)
         ions : list of str, optional
-          ions in the system, default None
-        concentration : float, optional
-          ionic concentration required, default None
+          ions in the system, default `None`
+        neutralize : bool, optional
+          if the net charge on the chemical state is neutralized by the ions in this
+          solvent component.  Default `True`
+        concentration : openff-units.unit.Quantity, optional
+          ionic concentration required, default `None`
+          this must be supplied with units, e.g. "1.5 * unit.molar"
         """
         self._smiles = smiles
         if ions is not None:
@@ -30,18 +39,27 @@ class SolventComponent:
                                       for i in ions))
         else:
             self._ions = tuple()
+        self._neutralize = neutralize
         self._concentration = concentration
 
     @property
-    def smiles(self):
+    def smiles(self) -> str:
+        """SMILES representation of the solvent molecules"""
         return self._smiles
 
     @property
-    def ions(self):
+    def ions(self) -> Tuple[str]:
+        """The ions in the solvent state"""
         return self._ions
 
     @property
-    def concentration(self):
+    def neutralize(self) -> bool:
+        """If the solvent neutralizes the system overall"""
+        return self._neutralize
+
+    @property
+    def concentration(self) -> unit.Quantity:
+        """Concentration of ions in the solvent state"""
         return self._concentration
 
     def __eq__(self, other):
@@ -55,7 +73,13 @@ class SolventComponent:
     def __hash__(self):
         return hash((self.smiles, self.ions, self.concentration))
 
+    @classmethod
+    def from_dict(cls, d):
+        """Deserialize from dict representation"""
+        return cls(**d)
+
     def to_dict(self):
-        """For serialisation"""
+        """For serialization"""
         return {'smiles': self.smiles, 'ions': self.ions,
-                'concentration': self.concentration}
+                'concentration': self.concentration,
+                'neutralize': self._neutralize}
