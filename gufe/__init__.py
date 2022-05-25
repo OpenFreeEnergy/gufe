5e5ba11872e8feb8f25a47da8de9b8beeda481ff
richard <richardjgowers@gmail.com>
2022-05-25 15:20:47 +0100

    made ProtocolDAGResult visible

--- gufe/__init__.py
+++ gufe/__init__.py
@@ -10,7 +10,14 @@ from .solventcomponent import SolventComponent
 
 from .chemicalsystem import ChemicalSystem
 
-from .protocols import Protocol, ProtocolResult, ProtocolDAG, ProtocolUnit
+from .protocols import (
+    Protocol,  # description of a method
+    ProtocolUnit,  # the individual step within a method
+    ProtocolDAG,  # many Units forming a workflow
+    ProtocolUnitResult,  # the result of a single Unit
+    ProtocolDAGResult,  # the collected result of a DAG
+    ProtocolResult,  # potentially many DAGs together, giving an estimate
+)
 from .mapping import Mapping
 from .transformations import Transformation, NonTransformation
 
