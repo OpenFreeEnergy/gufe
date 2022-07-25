310e192cf31682922269c22d626c6f55d670ccbe
riesben <benjamin-ries@outlook.com>
2022-07-25 18:28:35 +0200

    at least a bit of pep8

--- gufe/settings/models/chemical_composition.py
+++ gufe/settings/models/chemical_composition.py
@@ -1,47 +1,53 @@
 """
-Pydantic models used for storing settings.
+Pydantic models used for storing settings for chemical Composition.
 """
 
 from .abstract import SettingsBaseModel
 from pydantic import BaseModel
 from typing import List, Optional
 
-#abstract entity
+# abstract entity
 class chemical_entity(SettingsBaseModel):
-    exists:bool=False
-    
+    exists: bool = False
+
+
 # Solutes:
 class solute(chemical_entity):
-    num_compounds:int = 0
+    num_compounds: int = 0
 
-# Protein:        
+
+# Protein:
 class protein_cofactors(chemical_entity):
     metals: bool = False
-    organics:bool  = False
+    organics: bool = False
+
 
 class protein(chemical_entity):
-    num_residues:int = 0
-    biological_assembly:bool  = False
-    cofactors:protein_cofactors  = protein_cofactors()
+    num_residues: int = 0
+    biological_assembly: bool = False
+    cofactors: protein_cofactors = protein_cofactors()
 
-#Solvent:
+
+# Solvent:
 class solvent(chemical_entity):
-    num_solvent_molecules:int=0
-    num_ions:int=0
-    ion_types:List[str]=[]
-    solvent_type:str=None    
+    num_solvent_molecules: int = 0
+    num_ions: int = 0
+    ion_types: List[str] = []
+    solvent_type: str = None
 
-#Membrane:
+
+# Membrane:
 class membrane(chemical_entity):
-    num_lipids:int=0
-    lipid_composition:List[str]=[]
- 
-#Collecting Chem Comp   
+    num_lipids: int = 0
+    lipid_composition: List[str] = []
+
+
+# Collecting Chem Comp
 class chemical_composition(SettingsBaseModel):
     # What does this look like?
     # We take in smiles or file?
     # Or do we take in a SmallMoleculeComponent object?
     solute: solute = solute()
-    protein: protein  = protein()
-    solvent: solvent  = solvent()
+    protein: protein = protein()
+    solvent: solvent = solvent()
     membrane: membrane = membrane()
