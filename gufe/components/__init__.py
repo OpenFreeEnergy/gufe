e6fd55409e093649e9cbd3143c6edd3c08ed0194
David Dotson <dotsdl@gmail.com>
2022-07-27 10:20:33 -0700

    Reorganized component modules within a submodule of their own
    This is just an internal reorganization. Component classes such as
    `SmallMoleculeComponent` are still exposed at the `gufe` top level,
    allowing for e.g.:
    
    ```
    from gufe import SmallMoleculeComponent
    ```

---
+++ gufe/components/__init__.py
@@ -0,0 +1,5 @@
+from .component import Component
+
+from .smallmoleculecomponent import SmallMoleculeComponent
+from .proteincomponent import ProteinComponent
+from .solventcomponent import SolventComponent
