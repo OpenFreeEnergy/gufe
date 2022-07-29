0e70ce322e994f0a73b981085753de38da747877
David W.H. Swenson <david.swenson@omsf.io>
2022-07-29 12:59:03 -0500

    move transformation/network fixtures to conftest

--- gufe/tests/test_network.py
+++ gufe/tests/test_network.py
@@ -10,76 +10,7 @@ from .test_protocol import DummyProtocol, DummyProtocolResult
 from .test_tokenization import GufeTokenizableTestsMixin
 
 
-@pytest.fixture
-def benzene_variants_star_map(
-    benzene,
-    toluene,
-    phenol,
-    benzonitrile,
-    anisole,
-    benzaldehyde,
-    styrene,
-    prot_comp,
-    solv_comp,
-):
-
-    variants = [toluene, phenol, benzonitrile, anisole, benzaldehyde, styrene]
-
-    # define the solvent chemical systems and transformations between benzene and the others
-    solvated_ligands = {}
-    solvated_ligand_transformations = {}
-
-    solvated_ligands["benzene"] = ChemicalSystem(
-        {"solvent": solv_comp, "ligand": benzene}, name="benzene-solvent"
-    )
-
-    for ligand in variants:
-        solvated_ligands[ligand.name] = ChemicalSystem(
-            {"solvent": solv_comp, "ligand": ligand}, name=f"{ligand.name}-solvnet"
-        )
-        solvated_ligand_transformations[("benzene", ligand.name)] = Transformation(
-            solvated_ligands["benzene"],
-            solvated_ligands[ligand.name],
-            protocol=DummyProtocol(settings=None),
-            mapping=None,
-        )
-
-    # define the complex chemical systems and transformations between benzene and the others
-    solvated_complexes = {}
-    solvated_complex_transformations = {}
-
-    solvated_complexes["benzene"] = ChemicalSystem(
-        {"protein": prot_comp, "solvent": solv_comp, "ligand": benzene},
-        name="benzene-complex",
-    )
-
-    for ligand in variants:
-        solvated_complexes[ligand.name] = ChemicalSystem(
-            {"protein": prot_comp, "solvent": solv_comp, "ligand": ligand},
-            name=f"{ligand.name}-complex",
-        )
-        solvated_complex_transformations[("benzene", ligand.name)] = Transformation(
-            solvated_complexes["benzene"],
-            solvated_complexes[ligand.name],
-            protocol=DummyProtocol(settings=None),
-            mapping=None,
-        )
-
-    return AlchemicalNetwork(
-        list(solvated_ligand_transformations.values())
-        + list(solvated_complex_transformations.values())
-    )
-
-
 class TestAlchemicalNetwork(GufeTokenizableTestsMixin):
-
-    cls = AlchemicalNetwork
-    key = "AlchemicalNetwork-9c1d3f9ebc8e4d56d7347d59b0bda93c"
-
-    @pytest.fixture
-    def instance(self, benzene_variants_star_map):
-        return benzene_variants_star_map
-
     def test_init(self, benzene_variants_star_map):
         alnet = benzene_variants_star_map
 
