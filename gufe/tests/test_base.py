e0a0aa4243e1282b65d7a42fff478256dfa32b30
David W.H. Swenson <david.swenson@omsf.io>
2022-08-04 11:42:18 -0500

    first few tests for serialization base

--- gufe/tests/test_base.py
+++ gufe/tests/test_base.py
@@ -8,6 +8,13 @@ class Leaf(GufeTokenizable):
         self.a = a
         self.b = b
 
+    def __eq__(self, other):
+        return (
+            isinstance(other, self.__class__)
+            and self.a == other.a
+            and self.b == other.b
+        )
+
     def _to_dict(self):
         return {"a": self.a}
 
@@ -28,6 +35,14 @@ class Container(GufeTokenizable):
         self.lst = lst
         self.dct = dct
 
+    def __eq__(self, other):
+        return (
+            isinstance(other, self.__class__)
+            and self.obj == other.obj
+            and self.lst == other.lst
+            and self.dct == other.dct
+        )
+
     def _to_dict(self):
         return {'obj': self.obj, 'lst': self.lst, 'dct': self.dct}
 
@@ -43,28 +58,68 @@ class Container(GufeTokenizable):
 
 
 class TestGufeTokenizable:
-
-    def test_init(self):
+    def setup(self):
         leaf = Leaf("foo")
         bar = Leaf(leaf)
 
-        cont = Container(bar, [leaf, 0], {"leaf": leaf, "a": "b"})
+        self.cont = Container(bar, [leaf, 0], {"leaf": leaf, "a": "b"})
+        def leaf_dict(a):
+            return {'__module__': __name__, '__qualname__': "Leaf", "a": a}
 
+        self.expected_deep = {
+            '__qualname__': "Container",
+            '__module__': __name__,
+            'obj': leaf_dict(leaf_dict("foo")),
+            'lst': [leaf_dict("foo"), 0],
+            'dct': {"leaf": leaf_dict("foo"), "a": "b"}
+        }
 
-    def test_to_dict(self):
+        self.expected_shallow = {
+            '__qualname__': "Container",
+            '__module__': __name__,
+            'obj': bar,
+            'lst': [leaf, 0],
+            'dct': {'leaf': leaf, 'a': 'b'},
+        }
+
+        self.expected_keyed = {
+            '__qualname__': "Container",
+            '__module__': __name__,
+            'obj': {":gufe-key:": bar.key},
+            'lst': [{":gufe-key:": leaf.key}, 0],
+        'dct': {'leaf': {":gufe-key:": leaf.key}, 'a': 'b'}
+        }
+
+    def test_to_dict_deep(self):
+        assert self.cont.to_dict() == self.expected_deep
+
+    def test_from_dict_deep(self):
+        pytest.skip("TODO: figure out why this doesn't work")
         ...
+        recreated = Container.from_dict(self.expected_deep)
+        assert recreated == self.cont
 
     def test_to_dict_roundtrip(self):
         ...
 
     def test_to_keyed_dict(self):
+        assert self.cont.to_keyed_dict() == self.expected_keyed
+
+    def test_from_keyed_dict(self):
         ...
 
     def test_to_keyed_dict_roundtrip(self):
         ...
 
     def test_to_shallow_dict(self):
-        ...
+        assert self.cont.to_shallow_dict() == self.expected_shallow
+
+    def test_from_shallow_dict(self):
+        recreated = Container.from_shallow_dict(self.expected_shallow)
+        assert recreated == self.cont
+        # here we keep the same objects in memory
+        assert recreated.obj.a is recreated.lst[0]
+        assert recreated.obj.a is recreated.dct['leaf']
 
     def test_to_shallow_dict_roundtrip(self):
         ...
