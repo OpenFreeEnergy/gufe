a36ba302d2d202fb5b21cab00ebc0a0c9afc7a1a
riesben <benjamin-ries@outlook.com>
2022-08-30 20:26:39 +0200

    openmmPDBFile -> rdkit II - WIP missing Bondorder and fcharge

--- gufe/components/proteincomponent.py
+++ gufe/components/proteincomponent.py
@@ -2,13 +2,21 @@
 # For details, see https://github.com/OpenFreeEnergy/gufe
 from openff.toolkit.topology import Molecule
 
-from rdkit.Chem.rdchem import Mol, Atom, Conformer, EditableMol
+from rdkit import Chem
+from rdkit.Chem.rdchem import Mol, Atom, Conformer, EditableMol, BondType
 from openmm.app import PDBFile
 
 from gufe.components.explicitmoleculecomponent import ExplicitMoleculeComponent
+from .subfiles.PDBFile import PDBFile
 
 
-
+bond_types = {  1 : BondType.SINGLE,
+                2 : BondType.DOUBLE,
+                3 : BondType.TRIPLE ,
+               None :  BondType.UNSPECIFIED,
+               }
+          
+                    
 class ProteinComponent(ExplicitMoleculeComponent):
     """Wrapper around a Protein representation.
 
@@ -16,13 +24,14 @@ class ProteinComponent(ExplicitMoleculeComponent):
     do this in an appropriate toolkit then remake this class.
     """
     
-    def to_pdb(self) -> str:
+    def to_pdb(self, pdb_file_path:str) -> str:
         """Create a string based on SDF.
 
         This is the primary serialization mechanism for this class.
 
         """
-        pass
+        Chem.MolToPDBFile(pdb_file_path)
+        return pdb_file_path
     
     @classmethod
     def from_pdb_string(cls, pdbfile: str, name=""):
@@ -47,13 +56,19 @@ class ProteinComponent(ExplicitMoleculeComponent):
             _description_
         """
         openmm_PDBFile = PDBFile(pdbfile)
-        return cls.from_openmmPDBFile(openmm_PDBFile=openmm_PDBFile, name=name)
+        return cls._from_openmmPDBFile(openmm_PDBFile=openmm_PDBFile, name=name)
     
     
     @classmethod
-    def from_openmmPDBFile(cls, openmm_PDBFile:PDBFile, name:str):
+    def _from_openmmPDBFile(cls, openmm_PDBFile:PDBFile, name:str):
         """
-        This Function serializes openmmPDBFile
+        This Function serializes openmmPDBFile to 
+        AA - Protonations
+        
+        Test:        
+         - 1.5 serialization test
+         - check out files
+         - check obj
 
         Parameters
         ----------
@@ -68,48 +83,39 @@ class ProteinComponent(ExplicitMoleculeComponent):
             _description_
         """
             
+                    
+        periodicTable = Chem.GetPeriodicTable()
         mol_topology = openmm_PDBFile.topology
+
         rd_mol = Mol()
         editable_rdmol = EditableMol(rd_mol)
 
         # Build Topology
         # Add Atoms
-        for atomID, atom in enumerate(mol_topology.topology.atoms()):
+        for atom in mol_topology.atoms():
             atomID_orig = int(atom.index)
             a = Atom(atom.element.atomic_number)
             a.SetAtomMapNum(atomID_orig)
 
             a.SetProp("name", atom.name)
             a.SetIntProp("id", atomID_orig)
-            a.SetIntProp("formalCharge", 0) # WIP
 
             a.SetProp("resName", atom.residue.name)
             a.SetIntProp("resId", int(atom.residue.index))
 
             editable_rdmol.AddAtom(a)
 
-
         # Add Bonds
-        bondtypes = {1: BondType.SINGLE,
-                    2: BondType.DOUBLE,
-                    3: BondType.TRIPLE}
-
-        for bond in openmm_PDBFile.topology.bonds():
-            bond_order = bond.order # WIP
-            editable_rdmol.AddBond(bond.atom1.index, bond.atom2.index)#, bond_order)    
-
-        # Add Additionals
-        
-        #WIP
-
-
+        for bond in mol_topology.bonds():
+            bond_order = bond_types[bond.order]
+            editable_rdmol.AddBond(beginAtomIdx=bond.atom1.index, endAtomIdx=bond.atom2.index,)# order=bond_order)    
 
         # Set Positions
         rd_mol = editable_rdmol.GetMol()
         positions = list(map(list, openmm_PDBFile.positions._value))
         conf = Conformer(0)
         for atom_id, atom_pos in enumerate(positions):
-            conf.SetAtomPosition(atom_id, atom_pos)
+            conf.SetAtomPosition(atom_id, atom_pos) #unit: nm
         rd_mol.AddConformer(conf)
 
         # Molecule props
@@ -119,18 +125,50 @@ class ProteinComponent(ExplicitMoleculeComponent):
         rd_mol.SetIntProp("NumChains", mol_topology.getNumChains())
 
         # dimensions
-        pbcVs = list(map(list, mol_topology.getPeriodicBoxVectors()._value))
-        #UcellDim = list(map(list, top.getUnitCellDimensions()._value))
+        pbcVs = list(map(list, mol_topology.getPeriodicBoxVectors()._value)) #unit: nm
+        unitCellDim = list(map(float, mol_topology.getUnitCellDimensions()._value)) #unit: nm
+        rd_mol.SetProp("PeriodicBoxVectors", str(pbcVs))
+        rd_mol.SetProp("UnitCellDimensions", str(unitCellDim))
 
         # Sequence Settings
         residue_names = [r.name for r in mol_topology.residues()]
         res_seq = " ".join(residue_names) 
         rd_mol.SetProp("sequence", res_seq)
 
-
         # Chains
         rd_mol.SetProp("chain_names", str([c.index for c in mol_topology.chains()]))
         rd_mol.SetProp("chain_resi", str([list([r.index for r in c.residues()]) for c in mol_topology.chains()]))
+
+
+        # Add Additionals
+        # Set Bondorder
+        # WIP: if not possible above
+        
+        # Formal Charge
+        # WIP: I need Bondorder here!
+        atoms = rd_mol.GetAtoms()
+        netcharge = 0
+        for a in atoms:
+            a.UpdatePropertyCache()
+            connectivity = len(a.GetBonds()) #a.GetTotalValence()
+            atomic_num = a.GetAtomicNum()
+            default_valence = periodicTable.GetDefaultValence(atomic_num)
+            if(default_valence > connectivity):
+                fc = -(default_valence-connectivity) # negative charge
+            elif(default_valence < connectivity):
+                fc = +(connectivity-default_valence) # positive charge
+            else:
+                fc = 0 # neutral
+            
+            a.SetFormalCharge(fc)
+            #print(connectivity, default_valence, periodicTable.GetElementSymbol(atomic_num))
+
+            #if(fc > 0):
+            #    print(periodicTable.GetElementSymbol(atomic_num), fc, connectivity)
+            netcharge+=fc
+            
+        rd_mol.SetDoubleProp("NetCharge", netcharge)
+        rd_mol.UpdatePropertyCache()
         # Done
         
         return cls(rdkit=rd_mol, name=name)
