ce0d988da06d9e154bb99b35ec1e116b662dd1e1
David W.H. Swenson <david.swenson@omsf.io>
2022-10-27 15:25:44 -0500

    improved ensure_filelike

--- gufe/tests/test_transformation.py
+++ gufe/tests/test_transformation.py
@@ -3,8 +3,12 @@
 
 import pytest
 
+import io
+import pathlib
+
 from gufe.tests.test_tokenization import GufeTokenizableTestsMixin
 from gufe.transformations import Transformation, NonTransformation
+from gufe.transformations.transformation import ensure_filelike
 from gufe.protocols.protocoldag import execute
 
 from .test_protocol import DummyProtocol, DummyProtocolResult
@@ -24,6 +28,90 @@ def absolute_transformation(solvated_ligand, solvated_complex):
 @pytest.fixture
 def complex_equilibrium(solvated_complex):
     return NonTransformation(solvated_complex, protocol=DummyProtocol(settings=None))
+
+
+@pytest.mark.parametrize('input_type', [
+    "str", "path", "TextIO", "BytesIO", "StringIO"
+])
+def test_ensure_filelike(input_type, tmp_path):
+    path = tmp_path / "foo.txt"
+    use_bytes = input_type in {'path', 'BytesIO'}
+    filelike = input_type not in {'str', 'path'}
+    dumper = {
+        'str': str(path),
+        'path': path,
+        'TextIO': open(path, mode='w'),
+        'BytesIO': open(path, mode='wb'),
+        'StringIO': io.StringIO(),
+    }[input_type]
+
+    if filelike:
+        write_mode, read_mode = None, None
+    else:
+        if use_bytes:
+            write_mode, read_mode = "wb", "rb"
+        else:
+            write_mode, read_mode = "w", "r"
+
+    written = b"bar" if use_bytes else "bar"
+    with ensure_filelike(dumper, mode=write_mode) as write_f:
+        write_f.write(written)
+        write_f.flush()
+
+    if input_type == 'StringIO':
+        dumper.seek(0)
+
+    loader = {
+        'str': str(path),
+        'path': path,
+        'TextIO': open(path, mode='r'),
+        'BytesIO': open(path, mode='rb'),
+        'StringIO': dumper,
+    }[input_type]
+
+    with ensure_filelike(loader, mode=read_mode) as read_f:
+        loaded = read_f.read()
+
+    assert loaded == written
+
+    # we close pathlikes; do not close filelikes (by default)
+    assert write_f.closed is (not filelike)
+    assert read_f.closed is (not filelike)
+
+    # ensure everything is closed before we finish
+    write_f.close()
+    read_f.close()
+
+@pytest.mark.parametrize("input_type", ["TextIO", "BytesIO", "StringIO"])
+def test_ensure_filelike_force_close(input_type, tmp_path):
+    path = tmp_path / "foo.txt"
+    dumper = {
+        'TextIO': open(path, mode='w'),
+        'BytesIO': open(path, mode='wb'),
+        'StringIO': io.StringIO(),
+    }[input_type]
+    mode = "wb" if input_type == "BytesIO" else "w"
+    written = b"foo" if input_type == "BytesIO" else "foo"
+
+    with ensure_filelike(dumper, force_close=True) as f:
+        f.write(written)
+
+    assert f.closed
+
+
+@pytest.mark.parametrize("input_type", ["TextIO", "BytesIO", "StringIO"])
+def test_ensure_filelike_mode_warning(input_type, tmp_path):
+    path = tmp_path / "foo.txt"
+    dumper = {
+        'TextIO': open(path, mode='w'),
+        'BytesIO': open(path, mode='wb'),
+        'StringIO': io.StringIO(),
+    }[input_type]
+
+    with pytest.warns(UserWarning, match="User-specified mode will be ignored"):
+        _ = ensure_filelike(dumper, mode="w")
+
+    dumper.close()
 
 
 class TestTransformation(GufeTokenizableTestsMixin):
@@ -77,9 +165,10 @@ class TestTransformation(GufeTokenizableTestsMixin):
         )
         assert absolute_transformation == identical
 
-    def test_dict_roundtrip(self):
-        # TODO: need registration of `Protocol`s for this to work
-        ...
+    def test_dump_load_roundtrip(self, absolute_transformation):
+        string = io.StringIO()
+        pytest.skip()
+
 
 
 class TestNonTransformation(GufeTokenizableTestsMixin):
