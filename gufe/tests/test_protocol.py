4d0994a80f389b91c9dbfe2a3c35cdff213aad68
riesben <benjamin-ries@outlook.com>
2022-11-21 15:06:38 +0100

    adding unit-test

--- gufe/tests/test_protocol.py
+++ gufe/tests/test_protocol.py
@@ -368,7 +368,7 @@ class TestProtocol(GufeTokenizableTestsMixin):
 
         def test_protocol_unit_failures(self, instance: ProtocolDAGResult):
             assert len(instance.protocol_unit_failures) == 0
-            
+
         def test_protocol_unit_successes(self, instance: ProtocolDAGResult):
             assert len(instance.protocol_unit_successes) == 23
             assert all(isinstance(i, ProtocolUnitResult) for i in instance.protocol_unit_successes)
