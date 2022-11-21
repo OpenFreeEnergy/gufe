2ef78f5181f0fa2e8ff02b00a7af1bb4d8abcaf7
David W.H. Swenson <david.swenson@omsf.io>
2022-11-21 16:52:14 -0600

    update docs; add gufekey as prefix

--- gufe/tests/test_tokenization.py
+++ gufe/tests/test_tokenization.py
@@ -231,7 +231,8 @@ class TestGufeTokenizable(GufeTokenizableTestsMixin):
         fmt = logging.Formatter(
             "%(name)s - %(gufekey)s - %(levelname)s - %(message)s"
         )
-        logger = logging.getLogger('gufe.tests.test_tokenization.Leaf')
+        name = "gufekey.gufe.tests.test_tokenization.Leaf"
+        logger = logging.getLogger(name)
         logger.setLevel(getattr(logging, level))
         handler.setFormatter(fmt)
         logger.addHandler(handler)
@@ -239,7 +240,6 @@ class TestGufeTokenizable(GufeTokenizableTestsMixin):
         leaf = Leaf(10)
 
         results = stream.getvalue()
-        name = "gufe.tests.test_tokenization.Leaf"
         key = leaf.key.split('-')[-1]
 
         initial_log = f"{name} - UNKNOWN - INFO - no key defined!\n"
