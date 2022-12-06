c2f29449a4e97cec60886d24db85e2f467539a30
richard <richardjgowers@gmail.com>
2022-12-06 17:25:27 +0000

    TST: ensure that ProtocolUnit strictly has a hex key suffix

--- gufe/tests/test_protocol_unit.py
+++ gufe/tests/test_protocol_unit.py
@@ -1,3 +1,4 @@
+import string
 import pytest
 from pathlib import Path
 
@@ -40,3 +41,9 @@ class TestProtocolUnit(GufeTokenizableTestsMixin):
             # now try actually letting the error raise on execute
             with pytest.raises(ValueError, match="should always be 2"):
                 DummyUnit().execute(shared=Path('.'), raise_error=True, an_input=3)
+
+    def test_normalize(self, dummy_unit):
+        thingy = dummy_unit.key
+
+        assert thingy.startswith('DummyUnit-')
+        assert all(t in string.hexdigits for t in thingy.partition('-')[-1])
