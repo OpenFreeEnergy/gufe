112c69426a2a44f24762e48042040938a7225ca1
richard <richardjgowers@gmail.com>
2023-01-30 10:32:33 +0000

    move vis to gufe

--- gufe/mapping/__init__.py
+++ gufe/mapping/__init__.py
@@ -1,6 +1,7 @@
 # This code is part of gufe and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/gufe
 
+from . import mapping_visualization
 from .componentmapping import ComponentMapping
 from .atom_mapping import AtomMapping
 from .atom_mapper import AtomMapper
