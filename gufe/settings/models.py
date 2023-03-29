bb91a74ddd0508c5d09fa82695ae709840bc9006
IAlibay <ialibay@mdanalysis.org>
2023-03-29 14:44:21 +0100

    Clarify / fix some of the settings documentation

--- gufe/settings/models.py
+++ gufe/settings/models.py
@@ -26,7 +26,9 @@ class SettingsBaseModel(DefaultModel):
 class ThermoSettings(SettingsBaseModel):
     """Settings for thermodynamic parameters.
 
-    No checking is done to ensure a valid thermodynamic ensemble is possible.
+    .. note::
+       * No checking is done to ensure a valid thermodynamic ensemble is
+         possible.
     """
 
     temperature: FloatQuantity["kelvin"] = Field(
@@ -49,16 +51,25 @@ class BaseForcefieldSettings(SettingsBaseModel, abc.ABC):
 class OpenMMSystemGeneratorFFSettings(BaseForcefieldSettings):
     """Parameters to set up the forcefield with OpenMM Forcefields
 
-    Right now we just basically just grab what we need for the systemgenerator signature
-    https://github.com/openmm/openmmforcefields#automating-force-field-management-with-systemgenerator
+    .. note::
+       * Right now we just basically just grab what we need for the
+         :class:`openmmforcefields.system_generators.SystemGenerator`
+         signature. See the `OpenMMForceField SystemGenerator documentation`_
+         for more details.
+
+    
+    .. _`OpenMMForceField SystemGenerator documentation`:
+       https://github.com/openmm/openmmforcefields#automating-force-field-management-with-systemgenerator
     """
     constraints: Optional[str] = 'hbonds'
-    """one of 'hbonds', 'allbonds', 'hangles' or None, default 'hbonds'"""
+    """Constraints to be applied to system.
+       One of 'hbonds', 'allbonds', 'hangles' or None, default 'hbonds'"""
 
     rigid_water: bool = True
     remove_com: bool = False
     hydrogen_mass: float = 4.0
-    """the repartitioned hydrogen mass (in amu), default 4.0"""
+    """Mass to be repartitioned to hydrogens from neighbouring
+       heavy atoms (in amu), default 4.0"""
 
     forcefields: list[str] = [
         "amber/ff14SB.xml",  # ff14SB protein force field
@@ -66,10 +77,10 @@ class OpenMMSystemGeneratorFFSettings(BaseForcefieldSettings):
         "amber/tip3p_HFE_multivalent.xml",  # for divalent ions
         "amber/phosaa10.xml",  # Handles THE TPO
     ]
-    """list of forcefield paths for all components except the small molecules"""
+    """List of force field paths for all components except :class:`SmallMoleculeComponent`s"""
 
     small_molecule_forcefield: str = "openff-2.0.0"  # other default ideas 'openff-2.0.0', 'gaff-2.11', 'espaloma-0.2.0'
-    """the forcefield for small molecules"""
+    """Name of the force field to be used for :class:`SmallMoleculeComponent`s"""
 
     @pydantic.validator('constraints')
     def constraint_check(cls, v):
@@ -84,7 +95,7 @@ class Settings(SettingsBaseModel):
     """
     Container for all settings needed by a protocol
 
-    This represents the minimal surface that all settigns objects will have.
+    This represents the minimal surface that all settings objects will have.
 
     Protocols can subclass this to extend this to cater for their additional settings.
     """
