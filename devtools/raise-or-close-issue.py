363848d1ea4417469c5df4eee5d3cdeab57699d7
IAlibay <ialibay@mdanalysis.org>
2023-04-10 18:09:15 +0100

    Move things to a script

---
+++ devtools/raise-or-close-issue.py
@@ -0,0 +1,29 @@
+# A simple script to handle raising or closing a github issue
+# Is controlled by the following preset environment variables:
+#  - GITHUB_TOKEN: A github API token
+#  - CI_OUTCOME: The outcome status of a Github action step
+#  - TITLE: A string title which the issue will have.
+
+import os
+import numpy as np
+from github import Github
+
+
+if __name__ == "__main__":
+    git = Github(os.environ['GITHUB_TOKEN'])
+    status = os.environ['CI_OUTCOME']
+    repo = git.get_repo('OpenFreeEnergy/gufe')
+    title = os.environ['TITLE']
+    
+    target_issue = None
+    for issue in repo.get_issues():
+        if issue.title == title:
+            target_issue = issue
+    
+    # Close any issues with given title if CI returned green
+    if status == 'passed':
+        target_issue.edit(state='closed')
+    else:
+        # Otherwise raise an issue
+        if target_issue is None:
+            repo.create_issue(title=title, body="")
