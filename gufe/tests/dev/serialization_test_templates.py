dce68a6598bf5d986b5700ea43f410ac438eb195
Richard Gowers <richardjgowers@gmail.com>
2023-05-12 06:24:44 -0400

    ligandnetwork: add devtools to build test files

---
+++ gufe/tests/dev/serialization_test_templates.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# creates ligand_network.graphml
+
+
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from gufe import SmallMoleculeComponent, LigandNetwork, LigandAtomMapping
+
+
+def mol_from_smiles(smiles: str) -> Chem.Mol:
+    m = Chem.MolFromSmiles(smiles)
+    AllChem.Compute2DCoords(m)
+
+    return m
+
+
+# network_template.graphml
+mol1 = SmallMoleculeComponent(mol_from_smiles("CCO"))
+mol2 = SmallMoleculeComponent(mol_from_smiles("CC"))
+mol3 = SmallMoleculeComponent(mol_from_smiles("CO"))
+
+edge12 = LigandAtomMapping(mol1, mol2, {0: 0, 1: 1})
+edge23 = LigandAtomMapping(mol2, mol3, {0: 0})
+edge13 = LigandAtomMapping(mol1, mol3, {0: 0, 2: 1})
+
+network = LigandNetwork([edge12, edge23, edge13])
+
+with open("ligand_network.graphml", "w") as fn:
+    fn.write(network.to_graphml())
