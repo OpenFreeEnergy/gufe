4240960acecd7c4f64612f431b483664a431bbd8
riesben <benjamin-ries@outlook.com>
2024-04-17 21:51:33 +0200

    This is a start for a generic ligand network planner.

---
+++ gufe/ligand_network_planner.py
@@ -0,0 +1,48 @@
+import abc
+from typing import Iterable
+
+from . import SmallMoleculeComponent, LigandNetwork
+from . import AtomMapper
+from .mapping.atom_mapping_scorer import AtomMappingScorer
+
+class LigandNetworkPlanner(abc.ABC):
+    """A generic class for calculating :class:`.LigandNetworks`.
+
+    Implementations of this class can require an arbitrary and non-standardised
+    number of input arguments to create.
+
+    Implementations of this class provide the :meth:`.get_score` method
+
+    """
+
+    def __init__(self, mapper: AtomMapper, scorer:AtomMappingScorer, *args, *kwargs):
+        """ Generate a Ligand Network Planner. This class in general needs a mapper and a scorer.
+
+        Parameters
+        ----------
+        mapper: AtomMapper
+        scorer: AtomMappingScorer
+        args
+        kwargs
+        """
+        self.mapper = mapper
+        self.scorer =  scorer
+
+
+    def __call__(self, *args, **kwargs)-> LigandNetwork:
+        return self.generate_ligand_network(*args, **kwargs)
+
+    @abc.abstractmethod
+    def generate_ligand_network(self, ligands: Iterable[SmallMoleculeComponent])->LigandNetwork:
+        """Plan a Network which connects all ligands with minimal cost
+
+        Parameters
+        ----------
+        ligands : Iterable[SmallMoleculeComponent]
+        the ligands to include in the Network
+
+        Returns
+        -------
+        LigandNetwork
+            A Network, that connects all ligands with each other.
+        """
