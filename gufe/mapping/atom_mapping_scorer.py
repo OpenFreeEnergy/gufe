24a1081caf7ec5d5468a55b551d8e758aedad0ff
Benjamin Ries <benjamin-ries@outlook.com>
2024-08-27 17:30:50 +0200

    Update atom_mapping_scorer.py

--- gufe/mapping/atom_mapping_scorer.py
+++ gufe/mapping/atom_mapping_scorer.py
@@ -25,7 +25,7 @@ class AtomMappingScorer(GufeTokenizable):
     def get_score(self, mapping: AtomMapping) -> float:
         """ calculate the score for an  :class:`.AtomMapping`
             the scoring function returns a value between 0 and 1.
-            a value close to 1.0 indicates a small distance, a score close to zero indicates a large cost/error.
+            a value close to 1.0 indicates a small change, a score close to zero indicates a large cost/change.
 
         Parameters
         ----------
