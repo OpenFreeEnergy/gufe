1c8bf8cc4ed03bef6a912181bba492f8394c6899
Ian Kenney <ianmichaelkenney@gmail.com>
2024-10-31 13:58:45 -0700

    Added more tests for the ProtocolResult
    Explicitly test `get_uncertainty`, `get_estimate`, and `n_protocol_dag_results`

--- gufe/tests/test_protocolresult.py
+++ gufe/tests/test_protocolresult.py
@@ -9,16 +9,16 @@ from .test_tokenization import GufeTokenizableTestsMixin
 
 class DummyProtocolResult(gufe.ProtocolResult):
     def get_estimate(self):
-        return self.data['estimate']
+        return self.data["estimate"]
 
     def get_uncertainty(self):
-        return self.data['uncertainty']
+        return self.data["uncertainty"]
 
 
 class TestProtocolResult(GufeTokenizableTestsMixin):
     cls = DummyProtocolResult
-    key = 'DummyProtocolResult-b7b854b39c1e37feabec58b4000680a0'
-    repr = f'<{key}>'
+    key = "DummyProtocolResult-38c5649090dae613b2570c28155cc5fa"
+    repr = f"<{key}>"
 
     @pytest.fixture
     def instance(self):
@@ -26,3 +26,27 @@ class TestProtocolResult(GufeTokenizableTestsMixin):
             estimate=4.2 * unit.kilojoule_per_mole,
             uncertainty=0.2 * unit.kilojoule_per_mole,
         )
+
+    def test_protocolresult_get_estimate(self, instance):
+        assert instance.get_estimate() == 4.2 * unit.kilojoule_per_mole
+
+    def test_protocolresult_get_uncertainty(self, instance):
+        assert instance.get_uncertainty() == 0.2 * unit.kilojoule_per_mole
+
+    def test_protocolresult_default_n_protocol_dag_results(self, instance):
+        assert instance.n_protocol_dag_results == 0
+
+    @pytest.mark.parametrize(
+        "arg, expected", [(0, 0), (1, 1), (-1, ValueError)]
+    )
+    def test_protocolresult_get_n_protocol_dag_results_args(self, arg, expected):
+        try:
+            protocol_result = DummyProtocolResult(
+                n_protocol_dag_results=arg,
+                estimate=4.2 * unit.kilojoule_per_mole,
+                uncertainty=0.2 * unit.kilojoule_per_mole,
+            )
+            assert protocol_result.n_protocol_dag_results == expected
+        except ValueError:
+            if expected is not ValueError:
+                raise AssertionError()
