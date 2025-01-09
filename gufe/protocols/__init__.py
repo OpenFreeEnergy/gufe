d077d7adbe19179c86b1387ff55ed8ae352638e6
Josh Horton <Josh.Horton@newcastle.ac.uk>
2025-01-09 00:08:45 +0000

    Add `Protocol Errors` (#441)
    * first pass at protocol and results errors
    
    * fix error naming
    
    * fix docstring
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix tests
    
    * Update gufe/protocols/errors.py
    
    Co-authored-by: Irfan Alibay <IAlibay@users.noreply.github.com>
    
    * add base error, change MissingProtocolUnitError -> MissingUnitResultError
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Added news entry
    
    * Removed news entry; wrong branch :/
    
    * add feedback collapse protocol errors to a single unit execution error, add base results error
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Additional precision for error docstring
    
    * Added news entry
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Irfan Alibay <IAlibay@users.noreply.github.com>
    Co-authored-by: David L. Dotson <dotsdl@gmail.com>

--- gufe/protocols/__init__.py
+++ gufe/protocols/__init__.py
@@ -1,5 +1,13 @@
 """Defining processes for performing estimates of free energy differences"""
 
+from .errors import (
+    GufeProtocolError,
+    MissingUnitResultError,
+    ProtocolDAGResultError,
+    ProtocolUnitExecutionError,
+    ProtocolUnitFailureError,
+    ProtocolValidationError,
+)
 from .protocol import Protocol, ProtocolResult
 from .protocoldag import ProtocolDAG, ProtocolDAGResult, execute_DAG
 from .protocolunit import Context, ProtocolUnit, ProtocolUnitFailure, ProtocolUnitResult
---
+++ gufe/protocols/errors.py
@@ -0,0 +1,28 @@
+# This code is part of OpenFE and is licensed under the MIT license.
+# For details, see https://github.com/OpenFreeEnergy/gufe
+
+
+class GufeProtocolError(Exception):
+    """The base gufe error that other errors should subclass."""
+
+
+# Protocol Errors
+class ProtocolValidationError(GufeProtocolError):
+    """Error when the protocol setup or settings can not be validated."""
+
+
+class ProtocolUnitExecutionError(GufeProtocolError):
+    """Error when executing a protocol unit."""
+
+
+# Protocol Results Errors
+class ProtocolDAGResultError(GufeProtocolError):
+    """Base error when dealing with DAG results."""
+
+
+class MissingUnitResultError(ProtocolDAGResultError):
+    """Error when a ProtocolDAGResult has no ProtocolUnitResult(s) for a given ProtocolUnit."""
+
+
+class ProtocolUnitFailureError(ProtocolDAGResultError):
+    """Error when a ProtocolDAGResult has only ProtocolUnitFailure(s) for a given ProtocolUnit."""
