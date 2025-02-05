1dbab56de4c6b3dac9d8b2de109818b0d9179f2f
Alyssa Travitz <alyssa.travitz@omsf.io>
2025-02-05 08:49:23 -0800

    add test for negative net charge

--- gufe/components/explicitmoleculecomponent.py
+++ gufe/components/explicitmoleculecomponent.py
@@ -63,7 +63,8 @@ def _check_partial_charges(mol: RDKitMol, logger=None) -> None:
         errmsg = f"Incorrect number of partial charges: {len(p_chgs)} " f" were provided for {mol.GetNumAtoms()} atoms"
         raise ValueError(errmsg)
 
-    if (sum(p_chgs) - Chem.GetFormalCharge(mol)) > 0.01:
+    if abs(sum(p_chgs) - Chem.GetFormalCharge(mol)) > 0.01:
+        import pdb;pdb.set_trace()
         errmsg = (
             f"Sum of partial charges {sum(p_chgs)} differs from " f"RDKit formal charge {Chem.GetFormalCharge(mol)}"
         )
