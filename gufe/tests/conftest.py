26b7bb8b97dd23b12532ff5437a0aacc2aad2daf
Alyssa Travitz <alyssa.travitz@omsf.io>
2025-03-28 14:10:35 -0700

    remove thrombin fixture, no corresponding file

--- gufe/tests/conftest.py
+++ gufe/tests/conftest.py
@@ -114,13 +114,6 @@ def all_settings_path():
     with importlib.resources.path("gufe.tests.data", "all_settings.json") as f:
         yield str(f)
 
-
-@pytest.fixture
-def PDB_thrombin_path():
-    with importlib.resources.path("gufe.tests.data", "thrombin_protein.pdb") as f:
-        yield str(f)
-
-
 @pytest.fixture
 def PDBx_181L_path():
     with importlib.resources.path("gufe.tests.data", "181l.cif") as f:
