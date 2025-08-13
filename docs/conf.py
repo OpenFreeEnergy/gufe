cbe1e39011936f3fa53ed1f1d896d004a0f22591
Alyssa Travitz <alyssa.travitz@omsf.io>
2025-08-12 19:06:54 -0700

    turn off some autodoc pydantic things for now

--- docs/conf.py
+++ docs/conf.py
@@ -68,6 +68,15 @@ autodoc_mock_imports = [
     "rdkit",
 ]
 
+# autodoc_pydantic settings
+autodoc_pydantic_show_config = False
+autodoc_pydantic_model_show_config = False
+autodoc_pydantic_model_show_config_summary = False
+autodoc_pydantic_show_validators = False
+autodoc_pydantic_model_show_validators = False
+autodoc_pydantic_field_show_alias = False
+autodoc_pydantic_model_show_json = False
+
 
 # -- Options for HTML output -------------------------------------------------
 
