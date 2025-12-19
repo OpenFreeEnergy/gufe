f42c5a199bf56a2b34d138188184f4f183781ca0
Alyssa Travitz <alyssa.travitz@omsf.io>
2025-12-18 17:10:31 -0800

    Revert "drop abc inheritance from ComponentMapping"
    This reverts commit 24c2b8dfd6bf2c27e6e0a11b38284ed5867c8b8c.

--- gufe/mapping/componentmapping.py
+++ gufe/mapping/componentmapping.py
@@ -1,11 +1,12 @@
 # This code is part of gufe and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/gufe
+import abc
 
 import gufe
 from gufe.tokenization import GufeTokenizable
 
 
-class ComponentMapping(GufeTokenizable):
+class ComponentMapping(GufeTokenizable, abc.ABC):
     """A relationship between two Components stating that they transform in some way
 
     For components that are atom-based is specialised to :class:`.AtomMapping`
