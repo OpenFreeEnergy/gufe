7cbc27cc482fe68a99fc6149fbe271232361529b
hannahbaumann <hbaumann@uci.edu>
2026-01-09 12:43:41 +0100

    Add to init

--- gufe/components/__init__.py
+++ gufe/components/__init__.py
@@ -1,6 +1,6 @@
 """The building blocks for defining systems"""
 
 from .component import Component
-from .proteincomponent import ProteinComponent, ProteinMembraneComponent
+from .proteincomponent import ProteinComponent, SolvatedPDBComponent, ProteinMembraneComponent
 from .smallmoleculecomponent import SmallMoleculeComponent
 from .solventcomponent import BaseSolventComponent, SolventComponent
