279b84748984797e6607e9f88ab2e9efa59e47b7
Alyssa Travitz <alyssa.travitz@omsf.io>
2026-02-05 15:20:42 -0800

    don't mutate the fixture

--- gufe/tests/test_archival.py
+++ gufe/tests/test_archival.py
@@ -25,20 +25,21 @@ class TestArchival(GufeTokenizableTestsMixin):
         for transformation in transformations:
             transformation_results.append([transformation, [pdr_from_transformation(transformation)]])
         metadata = {"test_meta_key": "test_meta_value", "meta_ordered": [3, 2, 1]}
-        return AlchemicalArchive(
+        instance = AlchemicalArchive(
             network=alchemical_network, transformation_results=transformation_results, metadata=metadata
         )
+        return instance
 
     def test_version(self, instance):
         # fixture will generate correct version
         assert instance.version_gufe == gufe.__version__
 
     def test_version_warning(self, instance):
-        instance.version_gufe = "0.0.0"
-        json = instance.to_json()
+        new_instance = instance.copy_with_replacements(version_gufe="0.0.0")
+        json = new_instance.to_json()
 
         with pytest.warns(UserWarning):
-            reconstructed = AlchemicalArchive.from_json(content=json)
+            _ = AlchemicalArchive.from_json(content=json)
 
     def test_metadata(self, instance):
         assert instance.metadata == {"test_meta_key": "test_meta_value", "meta_ordered": [3, 2, 1]}
