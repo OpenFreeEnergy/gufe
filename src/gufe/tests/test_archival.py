9457212571aca85c882cd8eb60b60b9b6440d5ac
Alyssa Travitz <alyssa.travitz@omsf.io>
2026-08-27 11:02:41 -0500

    update archive testing to allow for alphanumeric versions

--- src/gufe/tests/test_archival.py
+++ src/gufe/tests/test_archival.py
@@ -1,5 +1,6 @@
 import importlib
 import warnings
+from unittest import mock
 
 import pytest
 
@@ -35,30 +36,39 @@ class TestArchival(GufeTokenizableTestsMixin):
         # fixture will generate correct version
         assert instance.version_gufe == gufe.__version__
 
-    def test_version_warning(self, instance):
-        major, minor, patch = instance.version_gufe.split(".")[:3]
+    @pytest.mark.parametrize(
+        "archive_ver,current_ver,should_warn",
+        [
+            ("1.12.0", "1.13.0", True),
+            ("1.12.0", "1.12.1", False),
+            ("1.12.0a0", "1.12.0", False),
+            ("1.12.1", "1.13", True),
+            ("1.13", "2.0", True),
+        ],
+    )
+    def test_version_warning(
+        self,
+        instance,
+        archive_ver,
+        current_ver,
+        should_warn,
+    ):
 
-        def new_version_to_json(new_version):
+        def json_with_archive_ver(new_version):
             new_instance = instance.copy_with_replacements(version_gufe=new_version)
             return new_instance.to_json()
 
-        # differing major or minor versions issue warning
-        versions = ((major, str(int(minor) + 1), patch), (str(int(major) + 1), minor, patch))
-        for new_version in map(lambda vstr: ".".join(vstr), versions):
-            # new_instance = instance.copy_with_replacements(version_gufe=new_version)
-            json = new_version_to_json(new_version)
-
-            with pytest.warns(UserWarning):
-                _ = AlchemicalArchive.from_json(content=json)
-
-        # differing patch raises no warning
-        new_version = ".".join((major, minor, str(int(patch) + 1)))
-        json = new_version_to_json(new_version)
-
-        with warnings.catch_warnings(record=True) as w:
-            warnings.simplefilter("always")
-            _ = AlchemicalArchive.from_json(content=json)
-            assert len(w) == 0
+        # pretend this json was created with `archive_ver`
+        json = json_with_archive_ver(archive_ver)
+        with mock.patch("gufe.__version__", current_ver):
+            if should_warn:
+                with pytest.warns(UserWarning, match="try loading with"):
+                    _ = AlchemicalArchive.from_json(content=json)
+            else:
+                with warnings.catch_warnings(record=True) as w:
+                    warnings.simplefilter("always")
+                    _ = AlchemicalArchive.from_json(content=json)
+                    assert len(w) == 0
 
     def test_metadata(self, instance):
         assert instance.metadata == {"test_meta_key": "test_meta_value", "meta_ordered": [3, 2, 1]}
@@ -106,21 +116,3 @@ def test_regression_archive_serialization():
         assert len(pdrs) == 1
 
     assert archive.version_gufe == "1.7.1.dev46+gb75e1476f.d20260203"
-
-
-@pytest.mark.parametrize("version", ["1.12.0a0", "1.12.0-alpha.1", "1.12.0-beta"])
-def test_version_alphanumeric(alchem_network_benzene_variants, version):
-    alchemical_network = alchem_network_benzene_variants
-    transformations = sorted(list(alchemical_network.edges))
-    # create fake results for the transformations
-    transformation_results = []
-    for transformation in transformations:
-        transformation_results.append([transformation, [pdr_from_transformation(transformation)]])
-    metadata = {"test_meta_key": "test_meta_value", "meta_ordered": [3, 2, 1]}
-    archive = AlchemicalArchive(
-        network=alchemical_network,
-        transformation_results=transformation_results,
-        metadata=metadata,
-        version_gufe=version,
-    )
-    assert archive.version_gufe == version
