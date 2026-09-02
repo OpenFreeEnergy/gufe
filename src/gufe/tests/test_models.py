a2a3f127dc0dda086385cffbf58d1d297024b0e9
Irfan Alibay <IAlibay@users.noreply.github.com>
2026-09-03 00:57:53 +0100

    Improve documentation in OpenMMSystemGeneratorFFSettings & revert (#832)
    * Improve documentation in OpenMMSystemGeneratorFFSettings & revert offxml extension
    
    Improve documentation in OpenMMSystemGeneratorFFSettings & revert offxml extension
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * remove news entry
    
    * fix tests
    
    * fix test
    
    * more fixes
    
    ---------
    
    Co-authored-by: Alyssa Travitz <31974495+atravitz@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/gufe/tests/test_models.py
+++ src/gufe/tests/test_models.py
@@ -81,7 +81,7 @@ def test_settings_schema():
 def settings_validation_schema():
     val_schema = {
         "additionalProperties": False,
-        "description": "Parameters to set up the force field with OpenMM ForceFields\n\n.. note::\n   Currently, this stores what is needed for the\n   :class:`openmmforcefields.system_generators.SystemGenerator` signature.\n   See the `OpenMMForceField SystemGenerator documentation`_ for more details.\n\n\n.. _`OpenMMForceField SystemGenerator documentation`:\n   https://github.com/openmm/openmmforcefields#automating-force-field-management-with-systemgenerator",
+        "description": "Parameters to set up the force field with `OpenMMForceFields <https://github.com/openmm/openmmforcefields>`.\n\n.. note::\n   Currently, this stores what is needed for the\n   :class:`openmmforcefields.system_generators.SystemGenerator` signature.\n   See the `OpenMMForceFields SystemGenerator documentation`_ for more details.\n\n\n.. _`OpenMMForceFields SystemGenerator documentation`:\n   https://github.com/openmm/openmmforcefields#automating-force-field-management-with-systemgenerator",
         "properties": {
             "constraints": {
                 "anyOf": [{"enum": ["hbonds", "allbonds", "hangles"], "type": "string"}, {"type": "null"}],
@@ -103,7 +103,7 @@ def settings_validation_schema():
                 "type": "array",
             },
             "small_molecule_forcefield": {
-                "default": "openff-2.2.1.offxml",
+                "default": "openff-2.2.1",
                 "title": "Small Molecule Forcefield",
                 "type": "string",
             },
@@ -153,7 +153,7 @@ def expected_model_dump():
                 "amber/phosaa10.xml",
                 "amber/lipid17_merged.xml",
             ],
-            "small_molecule_forcefield": "openff-2.2.1.offxml",
+            "small_molecule_forcefield": "openff-2.2.1",
             "nonbonded_method": "PME",
             "nonbonded_cutoff": {"val": 1.1, "unit": "nanometer"},
         },
